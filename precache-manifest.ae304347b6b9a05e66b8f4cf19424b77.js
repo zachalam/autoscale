3afc104a10dbbbdc4bd3df9fafc10eb1892fff24
Zach Alam <zelphics@gmail.com>
2019-07-28 23:04:50 -0700

    Updates

---
+++ precache-manifest.ae304347b6b9a05e66b8f4cf19424b77.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "940e040227e2b12886e00db7719ed89d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b4d9597a0e309c83cf4",
+    "url": "/static/css/main.328b1e13.chunk.css"
+  },
+  {
+    "revision": "e0e66345440a739fee2e",
+    "url": "/static/js/2.83984445.chunk.js"
+  },
+  {
+    "revision": "4b4d9597a0e309c83cf4",
+    "url": "/static/js/main.6fe6358c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "593d32f1e82b8355d32a652a54e69808",
+    "url": "/static/media/cpu.593d32f1.png"
+  },
+  {
+    "revision": "453fd9e851991f0014682bf703c36df4",
+    "url": "/static/media/net.453fd9e8.png"
+  },
+  {
+    "revision": "63c78e1d6f4eb1459c438f0a67108146",
+    "url": "/static/media/ram.63c78e1d.png"
+  }
+]);
